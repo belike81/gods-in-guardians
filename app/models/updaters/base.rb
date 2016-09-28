8c471e63d898deaba522fefacb4dc3b74f2f8212
Tomasz Grabowski <belike81@gmail.com>
2016-09-28 11:15:56 +0200

    Add weapon updater

--- app/models/updaters/base.rb
+++ app/models/updaters/base.rb
@@ -1,5 +1,29 @@
-class Updaters::Base
-  def initialize(api = ApiWrapper.new)
-    @api = api
+require "#{Rails.root}/lib/api_wrapper"
+
+module Updaters
+  class Base
+    def initialize(users, api = ApiWrapper.new)
+      @users = users
+      @api = api
+    end
+
+    def update
+      character_stats = get_character_stats
+      Updaters::Pvp::WeaponKills.new(users, character_stats).update
+    end
+
+    private
+
+    def get_character_stats
+      @stats ||= updated_character_stats
+    end
+
+    def updated_character_stats
+      @stats = {}
+      @users.each do |user|
+        @stats[user.id] = @api.get_account_stats(user.membership_id)
+      end
+      @stats
+    end
   end
 end
