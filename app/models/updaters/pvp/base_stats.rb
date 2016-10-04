d30c1e1ef9e65b2980a1b94cf363ddaeaef5ece1
Tomasz Grabowski <belike81@gmail.com>
2016-10-04 13:19:05 +0200

    Add base stats updater and model

---
+++ app/models/updaters/pvp/base_stats.rb
@@ -0,0 +1,23 @@
+module Updaters
+  module Pvp
+    class BaseStats
+      def initialize(users, stats)
+        @users, @stats = users, stats
+      end
+
+      def update
+        @users.each do |user|
+          PvpBaseStats.create({
+                                  user: user,
+                                  kills: @stats[user.id]['mergedAllCharacters']['results']['allPvP']['allTime']['kills']['basic']['value'],
+                                  deaths: @stats[user.id]['mergedAllCharacters']['results']['allPvP']['allTime']['deaths']['basic']['value'],
+                                  kd: @stats[user.id]['mergedAllCharacters']['results']['allPvP']['allTime']['killsDeathsRatio']['basic']['value'],
+                                  suicides: @stats[user.id]['mergedAllCharacters']['results']['allPvP']['allTime']['suicides']['basic']['value'],
+                                  assists: @stats[user.id]['mergedAllCharacters']['results']['allPvP']['allTime']['assists']['basic']['value'],
+                                  most_kills: @stats[user.id]['mergedAllCharacters']['results']['allPvP']['allTime']['bestSingleGameKills']['basic']['value']
+                                })
+        end
+      end
+    end
+  end
+end
---
+++ db/migrate/20161004075940_create_pvp_base_stats.rb
@@ -0,0 +1,14 @@
+class CreatePvpBaseStats < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pvp_base_stats do |t|
+      t.integer :user_id, index: true
+      t.integer :kills
+      t.integer :deaths
+      t.decimal :kd
+      t.integer :suicides
+      t.integer :assists
+      t.integer :most_kills
+      t.timestamps
+    end
+  end
+end
