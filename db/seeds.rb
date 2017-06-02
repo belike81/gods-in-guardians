2a29b7be6276fe065b7a39cddfd305972611a122
Tomasz Grabowski <belike81@gmail.com>
2017-06-02 13:44:16 +0200

    Update seeds with new guardians and not create them if they exits

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ require "#{Rails.root}/lib/api_wrapper"
 
 # Get initial users from GGS
 
-GGS_USERNAMES = %w(belike81 kasta71 majki_pl ouara_of_death piotrlupicki wroobel237 sarzasty ropel432)
+GGS_USERNAMES = %w(belike81 kasta71 majki_pl ouara_of_death piotrlupicki wroobel237 sarzasty ropel432 czarny00pl w1chajster)
 
 GGS_USERNAMES.each do |username|
   user = ApiWrapper.new.get_user_by_name(username)
-  if user
+  if user && !User.where(name: user.first['displayName']).first
     User.create(name: user.first['displayName'], membership_id: user.first['membershipId'])
   end
 end
