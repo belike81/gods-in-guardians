8747269e51e3742b5982580958d964454af28844
belike81 <belike81@gmail.com>
2018-05-23 15:32:21 +0200

    Fix the table issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def index
     @type = params[:type] || "pvp"
     @users = User.all
+    @keys = %w( super granade melee auto_rifle fusion hand_canon pulse_rifle rocket_launcher scout_rifle shotgun sniper side_arm sword )
   end
 end
